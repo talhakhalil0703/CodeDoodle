449fd892b5f0107cfd38b053f90bcf65f7bd4174
Jase Pasay <jase.pasay@ucalgary.ca>
2021-01-10 13:07:15 -0800

    All front end work done as of now

---
+++ src/components/DownloadButton.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+/* 
+    Download button component 
+    Receives props: 
+        - extension: the extension of the file to be downloaded, appended to end of file name (myFile is defaut right now)
+        - fileType: file type to be displayed to the user on the actual button
+        - file: the contents of the file to be downloaded
+*/
+class DownloadButton extends Component {
+
+    /* 
+        Creates and downloads the file uses props:
+            - extension
+            - file
+    */
+    downloadFile = () => {
+
+        const editor = document.createElement("a");
+        const file = new Blob([this.props.file],
+            { type: 'text/plain;charset=utf-8' });
+        editor.href = URL.createObjectURL(file);
+        editor.download = "myFile." + this.props.extension;
+        document.body.appendChild(editor);
+        editor.click();
+    }
+
+    /* 
+        Renders the button, downloads appropriate file on click
+        Uses props:
+            - fileType
+    */
+    render() {
+        return (
+            <button onClick={this.downloadFile}>Download {this.props.fileType}</button>
+        );
+    }
+}
+
+export default DownloadButton;
---
+++ src/components/DragAndDropFiles.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class DragAndDropFiles extends Component {
+    constructor(props) {
+        super(props);
+
+        this.dropRef = React.createRef();
+    }
+
+    componentDidMount() {
+        const drop = this.dropRef.current;
+        drop.addEventListener('dragover', this.handleDragOver);
+        drop.addEventListener('drop', this.handleFileUpload);
+    }
+
+    handleDragOver(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    /* 
+        Gets file, parses out file information, extension, and filename 
+        Accepts text/plain (which includes c/c++) and application/json file types
+    */
+    handleFileUpload = (e) => {
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        const [file] = e.dataTransfer.files;
+        if (file) {
+
+            /* getting the extension of the file */
+            var length = file.name.length;
+            var ext_pos = file.name.indexOf('.') + 1;
+            var ext = file.name.slice(ext_pos, length);
+
+            /* reading and sending info to CodeDoodle component */
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                this.props.onUpload(e.target.result, ext, file.name);
+            }
+            reader.readAsText(file);
+        }
+    };
+
+    render() {
+        return (
+            <div ref={this.dropRef} className='editor'>
+                {this.props.children}
+            </div>
+        );
+    }
+
+}
+
+export default DragAndDropFiles;
