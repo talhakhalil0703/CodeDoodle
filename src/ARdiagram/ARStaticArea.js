3f7fd5cfee57ed86b4f531337f440f7de46cce10
jasepasay98 <71850680+jasepasay98@users.noreply.github.com>
2021-01-11 18:42:01 -0800

    Add files via upload
    use drawing components in ARdiagram and GeneralDiagram folders, ones found in components folder are mine and aren't being used right now

---
+++ src/ARdiagram/ARStaticArea.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+
+function ARStaticArea() {
+return (
+  <React.Fragment>
+    <h1>Static</h1>
+  </React.Fragment>
+  );
+}
+
+export default ARStaticArea;
