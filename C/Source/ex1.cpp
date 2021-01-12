f76e0c45b61fe8574c22c3abdcc09cfdc16ed090
Saferer <james.nguy@shaw.ca>
2021-01-12 03:43:09 -0700

    Reference implementation

--- C/Source/ex1.cpp
+++ C/Source/ex1.cpp
@@ -1,28 +1,43 @@
 #include <stdlib.h>
 #include <string.h>
+
+class Goodbye
+{
+public:
+    int another_number;
+    Goodbye(int x);
+};
+
+Goodbye::Goodbye(int x) : another_number(x)
+{
+}
+
 class Hello
 {
 public:
     int a_number;
     char name[24];
-    Hello(int x);
+    Goodbye bye;
+    Hello(int x, int y);
 };
 
-Hello::Hello(int x) : a_number(x)
+Hello::Hello(int x, int y) : a_number(x), bye(y)
 {
     strcpy(name, "NAME");
 }
 
-void foo()
+void foo(int &z)
 {
     int y = 0;
 }
 
 int main(int argc, char *argv[])
 {
-    Hello hi(2);
-    Hello *yo = new Hello(3);
+    Hello hi(2, 7);
+    Hello *yo = new Hello(3, 5);
     int *num_p = &(yo->a_number);
-    foo();
+    int *heap_number = new int(5);
+    int ref = 5;
+    foo(ref);
     return 0;
 }
