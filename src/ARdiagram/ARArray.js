db418f94ae01f38d0045c14028eac865df68ba12
talhakhalil0703 <tkhalil0703@gmail.com>
2021-02-04 01:55:42 -0700

    #13 Removed state from ArrayElement

--- src/ARdiagram/ARArray.js
+++ src/ARdiagram/ARArray.js
@@ -22,12 +22,11 @@ class ARArray extends Component {
 
     removeElement(id) { // Need to do double array traversal... mighe be slow?
         let value = this.props.value
-        // value.array[0] = value.array[0].filter(element => element.elementID !== id)
         value.array.forEach((singleArray, index, arrays) => {arrays[index] = arrays[index].filter(element => element.elementID !== id);})//foreach takes three arguments, the element, index, and the array itself
         console.log(value)
         this.props.onChange(value)
-        value.array.forEach((singleArray, index, arrays) => {if(singleArray.length === 0){arrays = arrays.splice(index, 1);}})
-        //TODO: Remove array if all elements are removed from it
+        value.array.forEach((singleArray, index, arrays) => {if(singleArray.length === 0){arrays = arrays.splice(index, 1);}})//If empty remove the array it self
+        //TODO: Remove array if all elements are removed from it, need parent to do this
     }
 
     changeElementValue(id, elementValue) { // Need to do double array traversal... might be slow? Not too many elements for it to be slow
@@ -80,16 +79,16 @@ class ARArray extends Component {
 
         return (
             <div className="array">
-                {this.props.value.array.map((item) => ( //Map only works with arrays, you can nest maps
+                {this.props.value.array.map((singleArray) => ( //Map only works with arrays, you can nest maps
                     <div className="mainArray">
-                    {item.map((ele) => (<ARArrayElement key={ele.elementID}
-                                                        id = {ele.elementID}
-                                                        name = {ele.elementValue}
-                                                        removeElement = {this.removeElement}
-                                                        changeElement={this.changeElementValue}                
-                    />))}
+                        {singleArray.map((element) => (<ARArrayElement key={element.elementID}
+                                                            id = {element.elementID}
+                                                            name = {element.elementValue}
+                                                            removeElement = {this.removeElement}
+                                                            changeElement={this.changeElementValue}                
+                        />))}
                     </div>
-                                                                            ))}
+                ))}
             </div>
         )
     }
--- src/ARdiagram/ARArrayElement.js
+++ src/ARdiagram/ARArrayElement.js
@@ -6,28 +6,18 @@ class ARArrayElement extends Component { // TODO Convert this to not use state
 
     constructor(props) {
         super(props)
-        this.state = {
-            name: this.props.name,
-            id: this.props.id
-        }
-
         this.handleChange = this.handleChange.bind(this)
-
-
     }
 
     handleChange(event) {
-        let value = event.target.value
-        this.setState({ name: value })
         this.props.changeElement(this.props.id, event.target.value)
-       
     }
 
     render() {
 
         return (
             <div className = "element">
-                <input type="text" value={this.state.name} onChange={this.handleChange} />
+                <input type="text" value={this.props.name} onChange={this.handleChange} />
                 <button onClick={() => this.props.removeElement(this.props.id)}>-</button> {/*You need to use ()=>f() as then it won't get called in creation*/}
 
             </div>
