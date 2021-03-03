dc242f4a40f4d7ff1844b7abee4d9ecb7fef436d
talhakhalil0703 <tkhalil0703@gmail.com>
2021-03-02 22:57:32 -0700

    Basic version of removing element from Array #22
    Does not remove from the redux stack, and does not remove the whole
    array if empty need to add checking

--- src/components/codeDoodle/StackSlice/changeArrayVariableFunction.js
+++ src/components/codeDoodle/StackSlice/changeArrayVariableFunction.js
@@ -1,39 +1,45 @@
 export default function changeArrayVariableFunction(state, action) {
-  let { value, arrayVariableID, variableID, removeElement } = action.payload;
+  const { value, arrayVariableID, variableID, removeElement } = action.payload;
   //if in local
-  state.stack.map((stackFrame) => {
-    stackFrame.local.map((variable) => {
+  state.stack.map((stackFrame, stackIndex) => {
+    stackFrame.local.map((variable, arrayIndex) => {
       if (variable.variableID === arrayVariableID) {
         variable.value.array.map((array) => {
-          if (removeElement) {
-            return array.filter((element) => element.elementID !== variableID);
-          } else {
-            array.map((element) => {
-              if (element.elementID === variableID) {
-                element.elementValue = value;
-              }
-              return element;
-            });
-          }
+          console.log("Adding in stack");
+          array.map((element, elementIndex) => {
+            if (removeElement) {
+              state.stack[stackIndex].local[arrayIndex].value.array.splice(
+                elementIndex
+              );
+            }
+            if (element.elementID === variableID) {
+              element.elementValue = value;
+            }
+            return element;
+          });
+
           return array;
         });
       }
       return variable;
     });
     //if in arguments
-    stackFrame.args.map((variable) => {
+    stackFrame.args.map((variable, arrayIndex) => {
       if (variable.variableID === arrayVariableID) {
         variable.value.array.map((array) => {
-          if (removeElement) {
-            array = array.filter((element) => element.elementID !== variableID);
-          } else {
-            array.map((element) => {
-              if (element.elementID === variableID) {
-                element.elementValue = value;
-              }
-              return element;
-            });
-          }
+          console.log("Adding in stack");
+          array.map((element, elementIndex) => {
+            if (removeElement) {
+              state.stack[stackIndex].args[arrayIndex].value.array.splice(
+                elementIndex
+              );
+            }
+            if (element.elementID === variableID) {
+              element.elementValue = value;
+            }
+            return element;
+          });
+
           return array;
         });
       }
